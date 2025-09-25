1e053d3269a9451cf4857df8f6e11ec2c7e375ef
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-09-25 13:56:41 -0600

    500 pokemon to render

--- src/pages/PokePage.jsx
+++ src/pages/PokePage.jsx
@@ -7,7 +7,7 @@ export function PokePage() {
   const [finePokemon, setFinePokemon] = useState("");
 
   useEffect(() => {
-    GetPokemonList(40)
+    GetPokemonList(500)
       .then((pokemonListResponse) => setPokemons(pokemonListResponse))
       .catch((error) => {
         console.error(`Fetch pokemon error ${error}`);
