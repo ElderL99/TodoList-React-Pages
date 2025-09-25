6130c8c22b3956cdaa5191429ad01f144c09d37f
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-09-25 13:51:56 -0600

    hasRouter added

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import { PokePage } from "./pages/PokePage.jsx";
 import PokemonDetail from "./pages/PokemonDetail.jsx";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div className="bg-black text-red-500 w-screen h-screen">
-        <div className="flex justify-center items-center h-screen">
-          <Link to="/pokemon">
-            <button className="border border-blue-500 rounded-xl p-4 hover:border hover:border-yellow-500">
-              Go PokeDex
-            </button>
-          </Link>
-        </div>
-      </div>
-    ),
-  },
-  {
-    path: "/pokemon",
-    element: <PokePage />,
-  },
-  {
-    path: "/pokemon/:name",
-    element: <PokemonDetail />,
-  },
-]);
+// Creamos un componente principal
+function Main() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="bg-black text-red-500 w-screen h-screen">
+              <div className="flex justify-center items-center h-screen">
+                <Link to="/pokemon">
+                  <button className="border border-blue-500 rounded-xl p-4 hover:border hover:border-yellow-500">
+                    Go PokeDex
+                  </button>
+                </Link>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/pokemon" element={<PokePage />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
+      </Routes>
+    </HashRouter>
+  );
+}
 
+// Renderizamos la app
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Main />
   </StrictMode>
 );
