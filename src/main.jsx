330394073a087fe0080a1e5dcee1951757e0a89c
ElderL99 <adan.lugo.barrientos@outlook.com>
2025-09-25 13:31:53 -0600

    bug urls v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage.jsx";
 import { PokePage } from "./pages/PokePage.jsx";
 import PokemonDetail from "./pages/PokemonDetail.jsx";
 
-/* import App from "./App.jsx"; */
-/* import AppRHF from "./AppRHF"; */
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <div className="bg-black text-red-500  w-screen h-screen">
-        <div className="flex justify-center items-center h-screen ">
-          <a href="/pokemon" className="hover:translate-x-96">
-            <button className="border  border-blue-500 rounded-xl p-4 ">
-              Go pokeDex
+      <div className="bg-black text-red-500 w-screen h-screen">
+        <div className="flex justify-center items-center h-screen">
+          <Link to="/pokemon">
+            <button className="border border-blue-500 rounded-xl p-4 hover:translate-x-2 transition-transform">
+              Go PokeDex
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     ),
@@ -35,5 +32,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
